300d198700640e9b0e7292c1478845094d14ce88
mitalrs <mitalsapkale1999@gmail.com>
2022-12-29 11:13:45 +0530

    ui work

--- FrontEnd/nft-starter-project-forked/src/App.jsx
+++ FrontEnd/nft-starter-project-forked/src/App.jsx
@@ -14,16 +14,16 @@ const TOTAL_MINT_COUNT = 50;
 
 
 const App = () => {
-  
+
   /*
   * Just a state variable we use to store our user's public wallet. Don't forget to import useState.
   */
   const [currentAccount, setCurrentAccount] = useState("");
-  
+
   /*
   * Gotta make sure this is async.
   */
-   const checkIfWalletIsConnected = async () =>  {
+  const checkIfWalletIsConnected = async () => {
     /*
     * First make sure we have access to window.ethereum
     */
@@ -35,9 +35,9 @@ const App = () => {
     } else {
       console.log("We have the ethereum object", ethereum);
     }
-      /*
-    * Check if we're authorized to access the user's wallet
-    */
+    /*
+  * Check if we're authorized to access the user's wallet
+  */
     const accounts = await ethereum.request({ method: 'eth_accounts' });
 
     /*
@@ -52,21 +52,21 @@ const App = () => {
     }
 
     //There, now the user will know if they are on the wrong network!
-    let chainId = await ethereum.request({ method: 'eth_chainId'     });
+    let chainId = await ethereum.request({ method: 'eth_chainId' });
     console.log("Connected to chain " + chainId);
 
-     // String, hex code of the chainId of the Rinkebey test network
-    const goerliChainId = "0x5"; 
+    // String, hex code of the chainId of the Rinkebey test network
+    const goerliChainId = "0x5";
     if (chainId !== goerliChainId) {
-     alert("You are not connected to the Goerli Test Network!");
+      alert("You are not connected to the Goerli Test Network!");
+    }
   }
-  }
-   
+
   /*
   * Implement your connectWallet method here
   */
   const connectWallet = async () => {
-    
+
     try {
       const { ethereum } = window;
 
@@ -84,26 +84,26 @@ const App = () => {
       * Boom! This should print out public address once we authorize Metamask.
       */
       console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]); 
+      setCurrentAccount(accounts[0]);
     } catch (error) {
       console.log(error);
     }
-    
+
   }
   //added new function
   const askContractToMintNft = async () => {
     const loaderAni = document.getElementById('status');
-  const CONTRACT_ADDRESS = "0xF560454704ddeBd3581f76CBBAe7049A42917D5B";
+    const CONTRACT_ADDRESS = "0xF560454704ddeBd3581f76CBBAe7049A42917D5B";
 
-  try {
-    const { ethereum } = window;
+    try {
+      const { ethereum } = window;
 
-    if (ethereum) {
-      const provider = new ethers.providers.Web3Provider(ethereum);
-      const signer = provider.getSigner();
-      const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
 
-      // THIS IS THE MAGIC SAUCE.
+        // THIS IS THE MAGIC SAUCE.
         // This will essentially "capture" our event when our contract throws it.
         // If you're familiar with webhooks, it's very similar to that!
         connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
@@ -112,32 +112,32 @@ const App = () => {
         });
 
 
-      console.log("Going to pop wallet now to pay gas...")
-      let nftTxn = await connectedContract.makeAnEpicNFT();
-      loaderAni.style.display="block";
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+        loaderAni.style.display = "block";
 
-      console.log("Mining...please wait.")
-      await nftTxn.wait();
-      
-      
-      console.log(`Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`);
-      loaderAni.style.display="none";
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
 
-    } else {
-      console.log("Ethereum object doesn't exist!");
+
+        console.log(`Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`);
+        loaderAni.style.display = "none";
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
     }
-  } catch (error) {
-    console.log(error)
   }
-}
-  
+
   // Render Methods
   const renderNotConnectedContainer = () => (
     <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
-  
+
   /*
   * This runs our function when the page loads.
   */
@@ -150,34 +150,37 @@ const App = () => {
       <div className="container">
         <div className="header-container">
           <nav className="nav-bar"><img src="/src/assets/icon.png" alt="" />
-          <g className="title">EthericNFT</g>
+            <g className="title">EthericNFT</g>
           </nav>
+        </div>
+        <div style={{padding: "100px"}}>
           <p className="header gradient-text">My NFT Collection</p>
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-           {currentAccount === "" ? (
+          {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
             <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
               Mint NFT
             </button>
           )}
-        
-        <button className="cta-button connect-wallet-button">
+          
+
+          <button className="cta-button connect-wallet-button">
             <a href="https://testnets.opensea.io/" target="_blank">NFTCollection</a>
-        </button>
+          </button>
 
-        <div id="status">
-        <div className="loader">
-          <div className="inner one"></div>
-         <div className="inner two"></div>
-         <div className="inner three"></div>
-         </div>
-        <span className="waiting-text">wait for mint</span>
-       </div>
 
-        </div>
+          <div id="status">
+            <div className="loader">
+              <div className="inner one"></div>
+              <div className="inner two"></div>
+              <div className="inner three"></div>
+            </div>
+            <span className="waiting-text">wait for mint</span>
+          </div>
+          </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
